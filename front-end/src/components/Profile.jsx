41a6776973513c4a744c0fcad29daff523dbc910
mr-who-0903 <ritikjain0903@gmail.com>
2022-12-29 20:21:42 +0530

    typos corrected

--- front-end/src/components/Profile.jsx
+++ front-end/src/components/Profile.jsx
@@ -69,7 +69,7 @@ const Profile = (props) => {
                       <option selected value="NA" >NA</option>
                       <option value="Male">Male</option>
                       <option value="Female">Female</option>
-                      <option value="I prefer not to say">I prefer not to say</option>
+                      <option value="I prefer not to say">Others</option>
                     </select>
                 </div>
                 <div className='form-group'>
@@ -119,7 +119,7 @@ const Profile = (props) => {
               <label><b>Contact Number</b></label>
               <p>{userInfo.contact}</p>
 
-              <label><b>Resindential Address</b></label>
+              <label><b>Residential Address</b></label>
               <p>{userInfo.residential_addr}</p>
           </div>
         </div>
